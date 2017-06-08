977b35e363125ab72159720d8c6c7f07d9ab6bc4
Rodrigo Iloro <rodrigo.iloro@automattic.com>
2017-06-08 00:07:16 -0300

    Comments: moves write dispatch request to sites/resources/replies/new

--- client/state/data-layer/wpcom/sites/index.js
+++ client/state/data-layer/wpcom/sites/index.js
@@ -5,9 +5,11 @@ import { mergeHandlers } from 'state/action-watchers/utils';
 import automatedTransfer from './automated-transfer';
 import comments from './comments';
 import media from './media';
+import resources from './resources';
 
 export default mergeHandlers(
 	automatedTransfer,
 	comments,
-	media
+	media,
+	resources,
 );

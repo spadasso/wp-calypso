ccc404012fa7e417961d8bee3dfbfca6db72f233
Rodrigo Iloro <rodrigo.iloro@automattic.com>
2017-06-14 15:45:31 -0300

    Comments: adds handlers to match the endpoint structure

--- client/state/data-layer/wpcom/sites/index.js
+++ client/state/data-layer/wpcom/sites/index.js
@@ -5,11 +5,11 @@ import { mergeHandlers } from 'state/action-watchers/utils';
 import automatedTransfer from './automated-transfer';
 import comments from './comments';
 import media from './media';
-import resources from './resources';
+import posts from './posts';
 
 export default mergeHandlers(
 	automatedTransfer,
 	comments,
 	media,
-	resources,
+	posts,
 );

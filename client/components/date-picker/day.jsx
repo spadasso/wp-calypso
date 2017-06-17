4e7b30a0a22700fd2a34b5061d695778d3bf4061
retrofox <rdsuarez@gmail.com>
2017-06-17 10:12:43 -0300

    date-picker: limit events number per tooltip

--- client/components/date-picker/day.jsx
+++ client/components/date-picker/day.jsx
@@ -15,16 +15,17 @@ import { CalendarEvent } from './event';
 class DatePickerDay extends Component {
 	static propTypes = {
 		date: PropTypes.object.isRequired,
-		events: PropTypes.array
+		events: PropTypes.array,
+		maxEventsPerTooltip: PropTypes.number,
 	};
 
 	static defaultProps = {
-		showTooltip: false
+		maxEventsPerTooltip: 8,
 	};
 
 	state = {
 		showTooltip: false,
-	}
+	};
 
 	isPastDay( date ) {
 		const today = this.props.moment().set( {
@@ -56,14 +57,16 @@ class DatePickerDay extends Component {
 		}
 
 		const label = this.props.translate(
-				'%(posts)d post',
-				'%(posts)d posts', {
-					count: this.props.events.length,
-					args: {
-						posts: this.props.events.length
-					}
+			'%(events)d event',
+			'%(events)d events', {
+				count: this.props.events.length,
+				args: {
+					events: this.props.events.length
 				}
-			);
+			}
+		);
+
+		const moreEvents = this.props.events.length - this.props.maxEventsPerTooltip;
 
 		return (
 			<Tooltip
@@ -74,15 +77,31 @@ class DatePickerDay extends Component {
 			>
 				<span>{ label }</span>
 				<hr className="date-picker__division" />
-				<ul>{ map( this.props.events, event =>
-					<li key={ event.id }>
-						<CalendarEvent
-							icon={ event.icon }
-							socialIcon={ event.socialIcon }
-							socialIconColor={ event.socialIconColor }
-							title={ event.title } />
-					</li>
-				) }</ul>
+				<ul>
+					{ map( this.props.events, ( event, i ) => ( i < this.props.maxEventsPerTooltip ) &&
+						<li key={ event.id }>
+							<CalendarEvent
+								icon={ event.icon }
+								socialIcon={ event.socialIcon }
+								socialIconColor={ event.socialIconColor }
+								title={ event.title } />
+						</li>
+					) }
+
+					{ ( moreEvents > 0 ) &&
+						<li>
+							{ this.props.translate(
+								'… and %(moreEvents)d more event',
+								'… and %(moreEvents)d more events', {
+									count: moreEvents,
+									args: {
+										moreEvents
+									}
+								}
+							) }
+						</li>
+					}
+				</ul>
 			</Tooltip>
 		);
 	}
